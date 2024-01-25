b17e90c5876ff855b2ae0dd9485d77f731fb2ecc
Rodrigo César <rodrigocnn@gmail.com>
2024-01-25 15:06:15 -0300

    chore: initial config typescript

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, resp) => {
+  return resp.json({ message: "Hello Word" });
+});
+
+app.listen(3333, () => {
+  console.log("Server is running on port 3333");
+});
