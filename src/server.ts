e0c45bfeca4ff91e509b95e4b72c01f73d799efd
Rodrigo César <rodrigocnn@gmail.com>
2024-01-25 15:37:46 -0300

    chore: add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,4 @@ app.get("/", (req, resp) => {
   return resp.json({ message: "Hello Word" })
 })
 
-app.listen(3333, () => {
-  console.log("Server is running on port 3333")
-})
+app.listen(3333, () => console.log("Server is running!"))
