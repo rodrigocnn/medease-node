dae9688fc5f6033881c8a877de261821a389a900
Rodrigo César <rodrigocnn@gmail.com>
2024-01-25 19:02:40 -0300

    chore(database): add migration to model Patient

---
+++ prisma/migrations/20240125215632_create_patient_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `Patient` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `birth` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(20) NOT NULL,
+    `rg` VARCHAR(20) NOT NULL,
+    `cpf` VARCHAR(20) NULL,
+    `address` VARCHAR(60) NOT NULL,
+    `district` VARCHAR(80) NOT NULL,
+    `city` VARCHAR(80) NOT NULL,
+    `state` VARCHAR(80) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
