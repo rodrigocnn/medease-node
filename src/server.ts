e17c8d88091daa8e8e7d46ffa7623b01a2190dd2
Rodrigo César <rodrigocnn@gmail.com>
2024-02-02 17:34:17 -0300

    chore: tsconfig to deploy

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,19 @@ import "./shared/container"
 import express from "express"
 import { router } from "./routes"
 import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const corsOptions = {
   origin: "http://localhost:3000",
   optionsSuccessStatus: 200, // algumas versões do navegador 204
 }
 
+const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 app.use(cors(corsOptions))
 app.use(router)
 
-app.listen(3333, () => console.log("Server is running!"))
+app.listen(PORT, () => console.log("Server is running!"))
