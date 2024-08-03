a9cb44751dd8aa9a35723d05e5007f61d74c0c22
Rodrigo César <rodrigocnn@gmail.com>
2024-08-03 15:41:44 -0300

    chore(db): add deletedAt to Professional  model

---
+++ prisma/migrations/20240803184055_add_to_delete_at_to_professional/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `deletedAt` on the `patient` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `deletedAt` on the `role` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+  - You are about to alter the column `deletedAt` on the `service` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
+
+*/
+-- AlterTable
+ALTER TABLE `patient` MODIFY `deletedAt` DATETIME NULL;
+
+-- AlterTable
+ALTER TABLE `professional` ADD COLUMN `deletedAt` DATETIME NULL;
+
+-- AlterTable
+ALTER TABLE `role` MODIFY `deletedAt` DATETIME NULL;
+
+-- AlterTable
+ALTER TABLE `service` MODIFY `deletedAt` DATETIME NULL;
